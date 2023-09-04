b46f559cf05fdd318dde22be1965bc90ce4104cd
abrberin <140027053+abrberin@users.noreply.github.com>
2023-09-04 11:34:21 -0500

    Add files via upload

---
+++ mongodb__test.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.rv83ky7.mongodb.net/?retryWrites=true&w=majority"
+connection_string = "mongodb+srv://admin:admin@cluster0.rv83ky7.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(url)
+db = client.pytech
+try:
+    client = MongoClient(connection_string)
+    print("--Pytech COllection List--")
+    print(db.list_collection_names())
+    client.close()
+except Exception as e:
+     print(f"Error: {e}")
