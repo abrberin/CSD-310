c0d4faf5b86ecaab8aaad42ed22d42628838b03f
unknown <babraham@my365.bellevue.edu>
2023-09-08 11:47:41 -0500

    module_6

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -7,9 +7,9 @@ collection = db["students"]
 find_one = {"_id": 1007}
 print (find_one)
 
-update_one =({"_id: 1007"}, {"$set": {"last_name":"Abraham"}})
+update_one = {"_id": 1007}
+update_operation = {"$set": {"last_name": "Abraham"}}
+collection.update_one(update_one, update_operation)
 
 post4 = {"_id": 1010, "first_name": "Sammy", "last_name": "Joe"}
-
-
 collection.insert_one(post4)
