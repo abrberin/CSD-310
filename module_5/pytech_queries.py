4441eb865f5a253ca7cb41879b97479017be72b2
unknown <babraham@my365.bellevue.edu>
2023-09-07 23:27:26 -0500

    module_5

--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -1,16 +1,9 @@
+import pymongo
 from pymongo import MongoClient
 client = MongoClient("mongodb+srv://admin:admin@cluster0.rv83ky7.mongodb.net/?retryWrites=true&w=majority")
-db = client.pytech
-students = db.students
-print("All Students:")
-all_students = students.find({})
-for student in all_students:
-    print(student)
-student_id_to_find = 1007
-print(f"\nStudent with student_id {student_id_to_find}:")
-found_student = students.find_one({"student_id": student_id_to_find})
-if found_student:
-    print(found_student)
-else:
-    print(f"No student found with student_id {student_id_to_find}")
-client.close()
+db = client["pytech"]
+collection = db["students"]
+
+results = collection.find({"_id":1007})
+for x in results:
+    print(x)
